0df97734349b36d34e11ce7337d0fb5ec5710407
jmo-bancaeticalat <154364672+jmo-bancaeticalat@users.noreply.github.com>
2024-01-02 11:05:23 -0300

    Decomposition of contact Btn

---
+++ src/pages/ContactBtn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="fixed-bottom-right q-pb-md q-pr-lg">
+    <q-fab
+      color="primary"
+      icon="keyboard_arrow_up"
+      label="Contactanos"
+      label-position="left"
+      direction="up"
+    >
+      <!-- Btn without functionality -->
+      <q-fab-action
+        color="secondary"
+        @click="onClickMail"
+        label="Mail"
+        icon="mail"
+      />
+      <!-- Btn without functionality -->
+      <q-fab-action
+        color="secondary"
+        @click="onClickWhatsapp"
+        label="Whatsapp"
+        icon="chat"
+      />
+    </q-fab>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactBtn",
+  methods: {
+    onClickMail() {
+      // lógica para la acción de correo
+    },
+    onClickWhatsapp() {
+      // lógica para la acción de Whatsapp
+    },
+  },
+};
+</script>
